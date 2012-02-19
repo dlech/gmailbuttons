078bb6379ed63753032f3ee60a629d6be5bb2239
dlech <extraspecialdave@gmail.com>
2012-02-18 22:24:44 -0600

    got Trash and Spam buttons working

--- chrome/content/gmailbuttonsOverlay.js
+++ chrome/content/gmailbuttonsOverlay.js
@@ -5,8 +5,8 @@ var gmailbuttons = {
     this.strings = document.getElementById("gmailbuttons-strings");	
   },
   
-  updateJunkSpamButtons: function() {
-	// get current message
+  GetMessageServer: function() {
+    // get current message
 	var hdr = gFolderDisplay.selectedMessage;
     // give up if no message selected or this is a dummy header
     if (!hdr || gMessageDisplay.isDummy) 
@@ -19,23 +19,37 @@ var gmailbuttons = {
 	var svr = fldr.server;
 	if (!svr) // folder does not have server?
 		return;
+	return svr;
+  },
+  
+  // returns true if message is in gmail imap
+  IsMessageGmailIMAP: function() {	
+	var gmailHostNames = ["imap.gmail.com", "imap.googlemail.com"]; // TODO - pull these to a config file
+	var svr = this.GetMessageServer();
+	if (svr) 
+	  return gmailHostNames.indexOf(svr.hostName) >= 0;
+	return false;
+  },
+  
+  updateJunkSpamButtons: function() {
+	
 	// get message-specific header buttons	
 	var deleteButton = document.getElementById("hdrTrashButton");
 	var trashButton = document.getElementById("gmailbuttons-trash-button");
 	var junkButton = document.getElementById("hdrJunkButton");
 	var spamButton = document.getElementById("gmailbuttons-spam-button");
 	
-	var gmailHostNames = ["imap.gmail.com", "imap.googlemail.com"]; // TODO - pull these to a config file
-	if (gmailHostNames.indexOf(svr.hostName) >= 0) { // this is a gmail account
+	if (this.IsMessageGmailIMAP()) { // this is a gmail imap account
 	  deleteButton.oldTooltipText = deleteButton.tooltipText;
 	  deleteButton.tooltipText = this.strings.getString("deleteButton.tooltip");
+	  // TODO hide the delete button based on preference
 	  trashButton.hidden = false;
 	  junkButton.hidden = true;
 	  spamButton.hidden = false;
 	} else { // this is not a gmail account
 	  if (deleteButton.oldTooltipText)
 	    deleteButton.tooltipText = deleteButton.oldTooltipText;
-	  trashButton.hidden = true;
+	  trashButton.hidden = true; // TODO hide trash button if we are in the [Gmail]/Trash folder
 	  junkButton.hidden = false;
 	  spamButton.hidden = true;
 	}
@@ -54,7 +68,41 @@ var gmailbuttons = {
 	onEndAttachments: function() {
 		// do nothing
 	}	
-  }
+  },
+
+  // moves the selected message to the [Gmail]/Trash folder
+  MoveToTrash: function(e) {
+    if (this.IsMessageGmailIMAP()) { // mesage is on gmail imap server
+	  var svr = this.GetMessageServer();
+	  var gmailFolder = svr.rootFolder.getChildNamed("[Gmail]");
+	  if (gmailFolder) {
+	    gmailTrashFolder = gmailFolder.getChildNamed("Trash");
+		if (gmailTrashFolder) {
+		  gFolderDisplay.hintAboutToDeleteMessages();
+          gDBView.doCommandWithFolder(nsMsgViewCommandType.moveMessages, gmailTrashFolder);
+		  //return; // otherwise show error mesage below
+		}
+      }
+	} // trash button should not be visivle if not a gmail imap message
+	// TODO may want error message here
+  },
+
+  // moves the selected message to the [Gmail]/Spam folder
+  MoveToSpam: function(e) {
+    if (this.IsMessageGmailIMAP()) { // mesage is on gmail imap server
+	  var svr = this.GetMessageServer();
+	  var gmailFolder = svr.rootFolder.getChildNamed("[Gmail]");
+	  if (gmailFolder) {
+	    gmailTrashFolder = gmailFolder.getChildNamed("Spam");
+		if (gmailTrashFolder) {
+		  gFolderDisplay.hintAboutToDeleteMessages();
+          gDBView.doCommandWithFolder(nsMsgViewCommandType.moveMessages, gmailTrashFolder);
+		  //return; // otherwise show error mesage below
+		}
+      }
+	} // trash button should not be visivle if not a gmail imap message
+	// TODO may want error message here
+  }    
 };
 
 // listen for initial window load event
