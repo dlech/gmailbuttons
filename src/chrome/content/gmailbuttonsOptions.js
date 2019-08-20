139a3e33e031d61fec30b0602217bd5ccf3ce1ea
David Lechner <david@lechnology.com>
2019-08-20 11:40:24 -0500

    fix preferences for TB 68
    Preference-related xul elements are removed, so replace with recommended elements.
    
    https://developer.thunderbird.net/add-ons/updates/tb68#less-than-prefwindow-greater-than-less-than-prefpane-greater-than-less-than-preferences-greater-than-and-less-than-preference-greater-than

---
+++ src/chrome/content/gmailbuttonsOptions.js
@@ -0,0 +1,5 @@
+Preferences.addAll([
+    { id: "extensions.gmailbuttons.showDeleteButton", type: "bool" },
+    { id: "extensions.gmailbuttons.showGmailLabels", type: "bool" },
+    { id: "extensions.gmailbuttons.showGmailInfo", type: "bool" },
+]);
